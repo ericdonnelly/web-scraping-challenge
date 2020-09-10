dcd803601811aebec894857965bf8908d21e9b2a
ericdonnelly <ericdonnelly601@gmail.com>
2020-09-10 00:53:57 -0500

    updates

--- scrape_mars.py
+++ scrape_mars.py
@@ -20,7 +20,7 @@ def init_browser():
 
 def scrape_mars():
     browser = init_browser()
-
+#nasa news url
     try:
         # Visit visitcostarica.herokuapp.com
         browser.visit(nasa_news_url)
@@ -31,19 +31,58 @@ def scrape_mars():
         html = browser.html
         soup = bs(html, "html.parser")
 
+        #a = soup.find('div', class_="list_text")
         news_title = soup.find('div', class_= "content_title").text
-
         news_paragraph = soup.find('div', class_= "rollover_description_inner").text
     except:
         news_title = 'abort mission'
         news_paragraph ='abort mission'
+#space image url
+    try:
+        # Visit visitcostarica.herokuapp.com
+        browser.visit(space_image_url)
+
+        time.sleep(1)
+
+        # Scrape page into Soup
+        html = browser.html
+        soup = bs(html, "html.parser")
+
+        featured_image = soup.find("img", class_="thumb")["src"]
+        featured_image_url ="https://jpl.nasa.gov"+ featured_image
+    except:
+        featured_image = 'abort mission'
+#hemisphere
+    try:
+        img_dict = {}
+        title = result.find("h3").text
+        link = result.find("div", class_="description").a["href"]
+        next_link = base_hemisphere_url + link
+        # Visit visitcostarica.herokuapp.com
+        browser.visit(next_link)
+
+        time.sleep(1)
+
+        # Scrape page into Soup
+        html = browser.html
+        soup = bs(html, "html.parser")
+
+        url = soup.find("img", class_="wide-image")["src"]
+
+        img_dict["title"] = title
+        img_dict["img_url"] = base_hemisphere_url + url
+    
+        hemisphere_image_urls.append(img_dict)
+    except:
+        hemisphere_image_urls = 'abort mission'
 
     # Store data in a dictionary
     mars_data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
+        "featured_image_url": featured_image_url,
+        "hemisphere_image_urls": hemisphere_image_urls,
     }
-
     # Close the browser after scraping
     browser.quit()
 
